f97e4e8e0270edea8e7c332622eb6250d3b9d7f8
mavn.bussiness <bussiness.mavn@gmail.com>
2026-03-14 11:29:39 +0300

    changed app and head css files

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,4 +13,5 @@
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
 @vite(['resources/css/app.css', 'resources/css/kwdt.css', 'resources/js/app.js'])
+@fluxStyles
 @fluxAppearance
