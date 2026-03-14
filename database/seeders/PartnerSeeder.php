ccc3fb268cf2102bb2fc9100dc93e0411ec22980
mavn.bussiness <bussiness.mavn@gmail.com>
2026-03-14 12:25:35 +0300

    fixed the seeder for partners by removing unnecessary stuff.

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -9,145 +9,103 @@ class PartnerSeeder extends Seeder
 {
     public function run(): void
     {
+        Partner::truncate();
+
         $partners = [
             [
                 'name' => 'World Forum for Fish Workers and Fish Harvesters',
-                'logo_url' => 'https://worldfisherforum.org/wp-content/uploads/2020/08/WFF-Logo-Final.png',
+                'logo_url' => 'https://images.squarespace-cdn.com/content/v1/66daa23ce2a9864d9d00cc45/0a79a067-a783-42f4-b8ee-fb06a514ef05/1+WFF.png?format=500w',
                 'website' => 'https://worldfisherforum.org/',
                 'description' => 'International network of fish workers and harvesters advocating for sustainable fisheries and social justice.',
                 'is_active' => true,
                 'order' => 1,
             ],
             [
-                'name' => 'Gender Water Alliance',
-                'logo_url' => 'https://genderandwater.org/logo-gwa.png/@@images/image.png',
-                'website' => 'https://genderandwater.org/',
-                'description' => 'Promotes women and men\'s equitable access to and management of safe water.',
+                'name' => 'Women For Water Partnership',
+                'logo_url' => 'https://womenforwater.org/wp-content/uploads/2023/02/logo-black.png',
+                'website' => 'https://womenforwater.org/projects-work/',
+                'description' => 'A partnership of women\'s organization and networks to unite women leadership in water and sanitation.',
                 'is_active' => true,
                 'order' => 2,
             ],
             [
-                'name' => 'Uganda National NGO Forum',
-                'logo_url' => 'https://ngoforum.or.ug/wp-content/uploads/2022/05/UNNGOF-Logo-Full-Color-1536x430.png',
-                'website' => 'https://ngoforum.or.ug/',
-                'description' => 'Provides a sharing and reflection platform for NGOs in Uganda.',
+                'name' => 'Wocan',
+                'logo_url' => 'https://www.wocan.org/wp-content/uploads/2023/06/wocan-logo.svg',
+                'website' => 'https://www.wocan.org/',
+                'description' => 'A women led international membership network of women and men professionals and women organizations that pioneers in gender equality and climate change.',
                 'is_active' => true,
                 'order' => 3,
             ],
             [
-                'name' => 'Uganda Women\'s Network (UWONET)',
-                'logo_url' => null, // No logo found
-                'website' => 'https://uwonet.or.ug/',
-                'description' => 'A network of organizations and individuals working towards gender equality and women\'s empowerment in Uganda.',
+                'name' => 'The Butterfly Effect',
+                'logo_url' => 'https://www.effetpapillon.org//public/static/project/css/images/logo_EN.png',
+                'website' => 'https://www.effetpapillon.org/en/',
+                'description' => 'A network of international and local CSO/NGO, from all over the globe, which advocates for effective local solutions that have a sustainable impact on improving access to water and sanitation and water resource management.',
                 'is_active' => true,
                 'order' => 4,
             ],
             [
-                'name' => 'Women of Uganda Network (WOUGNET)',
-                'logo_url' => 'https://wougnet.org/wp-content/uploads/2021/05/cropped-WOUGNET-LOGO-1.png',
-                'website' => 'https://wougnet.org/',
-                'description' => 'Develops use of information and communication technologies among women as tools to share information and address issues collectively.',
+                'name' => 'Gender Water Alliance',
+                'logo_url' => 'https://genderandwater.org/%2b%2btheme%2b%2bgwa.theme/images/logo.png',
+                'website' => 'https://genderandwater.org/en/',
+                'description' => 'Promotes women and men\'s equitable access to and management of safe water.',
                 'is_active' => true,
                 'order' => 5,
             ],
             [
-                'name' => 'Uganda National Women\'s Fish Organisation (UNWFO)',
-                'logo_url' => null, // No logo found
-                'website' => null,
-                'description' => 'Networks national women\'s organization in fisheries and aquaculture.',
+                'name' => 'Uganda National NGO Forum',
+                'logo_url' => 'https://ngoforum.or.ug/wp-content/uploads/2024/01/UNNGOFlogoBase.png',
+                'website' => 'https://ngoforum.or.ug/',
+                'description' => 'Provides a sharing and reflection platform for NGOs in Uganda.',
                 'is_active' => true,
                 'order' => 6,
             ],
-            // Additional existing/previous partners
             [
-                'name' => 'University of Northampton',
-                'logo_url' => 'https://upload.wikimedia.org/wikipedia/en/thumb/9/98/University_of_Northampton_logo.svg/1920px-University_of_Northampton_logo.svg.png',
-                'website' => 'https://www.northampton.ac.uk/',
-                'description' => 'UK-based university supporting KWDT through research and academic partnerships.',
+                'name' => 'Uganda Water and Sanitation NGO Network',
+                'logo_url' => 'https://uwasnet.org/wp-content/uploads/2024/04/uwasnet-logo-new.png',
+                'website' => 'https://uwasnet.org/',
+                'description' => 'National coordination of CSOs including social enterprises, development programs, private sector and NGOs on water and environment.',
                 'is_active' => true,
                 'order' => 7,
             ],
             [
-                'name' => 'Universität Hamburg',
-                'logo_url' => 'https://www.uni-hamburg.de/resources/logo/uh-logo.svg',
-                'website' => 'https://www.uni-hamburg.de/',
-                'description' => 'German university collaborating on research and community development projects.',
+                'name' => 'Uganda Women\'s Network',
+                'logo_url' => 'https://www.uwonet.or.ug/wp-content/themes/uwonet/images/uwonet-logo-with-tagline.png',
+                'website' => 'https://www.uwonet.or.ug/',
+                'description' => 'Uganda Women\'s Network (UWONET) is a women\'s rights advocacy organization that exists to coordinate collective action among women\'s rights and gender equality stakeholders for the attainment of gender equality and equity in Uganda.',
                 'is_active' => true,
                 'order' => 8,
             ],
             [
-                'name' => 'Uganda Management Institute',
-                'logo_url' => 'https://www.umi.ac.ug/wp-content/uploads/2021/02/UMI-Logo.png',
-                'website' => 'https://www.umi.ac.ug/',
+                'name' => 'Women of Uganda Network',
+                'logo_url' => 'https://wougnet.org/wp-content/uploads/2025/11/cropped-cropped-wougnet-logo.png',
+                'website' => 'https://wougnet.org/',
                 'description' => 'Ugandan center for management training and development.',
                 'is_active' => true,
                 'order' => 9,
             ],
             [
-                'name' => 'GIZ',
-                'logo_url' => 'https://www.giz.de/static/img/giz-logo.png',
-                'website' => 'https://www.giz.de/',
-                'description' => 'German development agency supporting sustainable development initiatives.',
+                'name' => 'Uganda National Women\'s Fish Organisation',
+                'logo_url' => 'https://unwfo.org/wp-content/uploads/2024/04/cropped-UNWFO-logo-2.png',
+                'website' => 'https://unwfo.org/',
+                'description' => 'UNWFO is a registered non-profit organization established in 2015 and officially launched in 2019 with the support of the Ministry of Agriculture Animal Industry and Fisheries. The Organization serves as a network for women and youth involved in fish production, processing, marketing and trade.',
                 'is_active' => true,
                 'order' => 10,
             ],
             [
-                'name' => 'ARCHE NOVA',
-                'logo_url' => 'https://arche-nova.org/wp-content/uploads/2022/01/ArcheNova-Logo.svg',
-                'website' => 'https://arche-nova.org/',
+                'name' => 'Mukono District NGO Forum',
+                'logo_url' => 'https://images.squarespace-cdn.com/content/v1/66daa23ce2a9864d9d00cc45/64f05fc1-386f-481f-9540-bfa708f11880/7+MUDINFO.jpeg?format=500w',
+                'website' => 'https://mukonongoforum.wordpress.com/',
                 'description' => 'German NGO supporting community-based development projects.',
                 'is_active' => true,
                 'order' => 11,
             ],
-            [
-                'name' => 'FIAN Uganda',
-                'logo_url' => 'https://www.fian.org/fileadmin/_processed_/e/f/csm_fian-logo_aaf3237f2f.png',
-                'website' => 'https://www.fian.org/',
-                'description' => 'Human rights organization working on the right to food and nutrition.',
-                'is_active' => true,
-                'order' => 12,
-            ],
-            [
-                'name' => 'Fokus Frauen',
-                'logo_url' => 'https://fokus-frauen.de/wp-content/uploads/2020/09/logo-ff.png',
-                'website' => 'https://fokus-frauen.de/',
-                'description' => 'German organization focusing on women\'s rights and empowerment.',
-                'is_active' => true,
-                'order' => 13,
-            ],
-            [
-                'name' => 'European Union Delegation to Uganda',
-                'logo_url' => 'https://european-union.europa.eu/logo-eu.svg',
-                'website' => 'https://www.eeas.europa.eu/delegations/uganda_en',
-                'description' => 'EU representation in Uganda supporting development cooperation.',
-                'is_active' => true,
-                'order' => 14,
-            ],
-            [
-                'name' => 'FAO',
-                'logo_url' => 'https://www.fao.org/logo/fao-logo.svg',
-                'website' => 'https://www.fao.org/',
-                'description' => 'Food and Agriculture Organization of the United Nations.',
-                'is_active' => true,
-                'order' => 15,
-            ],
-            [
-                'name' => 'NGO Bureau Uganda',
-                'logo_url' => 'https://ngobureau.go.ug/sites/default/files/logo.png',
-                'website' => 'https://ngobureau.go.ug/',
-                'description' => 'Government body regulating NGO operations in Uganda.',
-                'is_active' => true,
-                'order' => 16,
-            ],
         ];
-
-        // Optional: Clear existing partners (uncomment if you want to start fresh)
-        // Partner::truncate();
 
         foreach ($partners as $partner) {
             Partner::create($partner);
         }
 
-        $this->command->info('Successfully seeded '.count($partners).' partners.');
+        $this->command->info('Successfully seeded ' . count($partners) . ' partners.');
     }
 }
