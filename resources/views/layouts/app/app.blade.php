48b621fc2e8941f61ce1a0700bc768f47679c74e
mavn.bussiness <bussiness.mavn@gmail.com>
2026-04-01 17:15:46 +0300

    refactor: add `[x-cloak]` utility to tokens.css, rename class in intersection observer for clarity

--- resources/views/layouts/app/app.blade.php
+++ resources/views/layouts/app/app.blade.php
@@ -31,7 +31,7 @@
     const revealObserver = new IntersectionObserver((entries) => {
         entries.forEach((entry, i) => {
             if (entry.isIntersecting) {
-                setTimeout(() => entry.target.classList.add('revealed'), i * 80);
+                setTimeout(() => entry.target.classList.add('visible'), i * 80);
                 revealObserver.unobserve(entry.target);
             }
         });
