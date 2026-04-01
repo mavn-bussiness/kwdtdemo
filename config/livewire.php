282ba7010de356705c3c290dac428baed0bc9003
mavn.bussiness <bussiness.mavn@gmail.com>
2026-04-01 18:54:41 +0300

    config: add support for custom Livewire asset URL through `LIVEWIRE_ASSET_URL` env variable

--- config/livewire.php
+++ config/livewire.php
@@ -2,6 +2,7 @@
 
 return [
 
+    'asset_url' => env('LIVEWIRE_ASSET_URL', null),
     /*
     |---------------------------------------------------------------------------
     | Component Locations
