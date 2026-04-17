ecc029cb18f32a8461bfb5ffc7b76fb63a2db698
mavn.bussiness <bussiness.mavn@gmail.com>
2026-04-17 13:57:57 +0300

    fix: correct vite manifest path to build/.vite/manifest.json

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -12,7 +12,7 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@if(file_exists(public_path('build/manifest.json')))
+@if(file_exists(public_path('build/.vite/manifest.json')))
     @vite(['resources/css/app.css', 'resources/css/kwdt.css', 'resources/js/app.js'])
 @endif
 @php
