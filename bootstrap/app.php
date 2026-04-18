e8108303c775995813fcb7ba2f5764ca9f1f28c4
mavn.bussiness <bussiness.mavn@gmail.com>
2026-04-18 22:21:05 +0300

    fix: force session driver to database in booting() before session middleware initializes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->booting(function () {
+        if (app()->isProduction()) {
+            config(['session.driver' => 'database']);
+        }
+    })
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
